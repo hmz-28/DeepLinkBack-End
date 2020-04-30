b4ae547a20ca9ac59404d09a5385329fcc6fb586
Your Name <you@example.com>
2020-04-30 15:41:36 +0100

    first commit

---
+++ src/test/java/sm/deeplink/mainapp/DeepLinkBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package sm.deeplink.mainapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeepLinkBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
